39abf269810ff563c8d4bf06772d82b7ebd3a457
p.schwartenbeck <pschwartenbeck@gmail.com>
2026-01-30 00:59:07 +0100

    feat: add config (wip)

--- src/sft.py
+++ src/sft.py
@@ -11,80 +11,10 @@ import torch
 from datasets import load_dataset
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from trl import SFTConfig, SFTTrainer
+from sft_util import generate_responses, test_model_with_questions, load_model_and_tokenizer, display_dataset, load_config
+from sft_static import QUESTIONS
 
 warnings.filterwarnings('ignore')
-
-def generate_responses(model, tokenizer, user_message, system_message=None, max_new_tokens=100):
-    messages = []
-    if system_message:
-        messages.append({"role": "system", "content": system_message})
-    messages.append({"role": "user", "content": user_message})
-    prompt = tokenizer.apply_chat_template(
-        messages,
-        tokenize=False,
-        add_generation_prompt=False,
-        enable_thinking=False
-    )
-    inputs=tokenizer(prompt,return_tensors="pt").to(model.device)
-    # outputs = model.generate(inputs)
-    with torch.no_grad():
-        outputs=model.generate(
-            **inputs,
-            max_new_tokens=max_new_tokens,
-            do_sample=False,
-            pad_token_id=tokenizer.eos_token_id,
-            eos_token_id=tokenizer.eos_token_id
-        )
-
-    input_len = inputs["input_ids"].shape[1]
-    generated_ids=outputs[0][input_len:]
-    response=tokenizer.decode(generated_ids,skip_special_tokens=True).strip()
-    return response
-
-def test_model_with_questions(model, tokenizer, questions, 
-                              system_message=None, title="Model Output"):
-    print(f"\n=== {title} ===")
-    for i, question in enumerate(questions, 1):
-        response = generate_responses(model, tokenizer, question, 
-                                      system_message)
-        print(f"\nModel Input {i}:\n{question}\nModel Output {i}:\n{response}\n")
-
-def load_model_and_tokenizer(checkpoint, device):
-    # Load base model and tokenizer
-    tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-    model = AutoModelForCausalLM.from_pretrained(checkpoint).to(device)
-    # model = AutoModelForCausalLM.from_pretrained(checkpoint)
-    # if use_gpu:
-    #     model.to("cuda")
-    if not tokenizer.chat_template:
-        tokenizer.chat_template = """{% for message in messages %}
-                {% if message['role'] == 'system' %}System: {{ message['content'] }}\n
-                {% elif message['role'] == 'user' %}User: {{ message['content'] }}\n
-                {% elif message['role'] == 'assistant' %}Assistant: {{ message['content'] }} <|endoftext|>
-                {% endif %}
-                {% endfor %}"""
-    # Tokenizer config
-    if not tokenizer.pad_token:
-        tokenizer.pad_token = tokenizer.eos_token
-    return model, tokenizer
-
-def display_dataset(dataset):
-    # Visualize the dataset 
-    rows = []
-    for i in range(3):
-        example = dataset[i]
-        user_msg = next(m['content'] for m in example['messages']
-                        if m['role'] == 'user')
-        assistant_msg = next(m['content'] for m in example['messages']
-                             if m['role'] == 'assistant')
-        rows.append({
-            'User Prompt': user_msg,
-            'Assistant Response': assistant_msg
-        })
-    # Display as table
-    df = pd.DataFrame(rows)
-    pd.set_option('display.max_colwidth', None)  # Avoid truncating long strings
-    print(df)
 
 # SFTTrainer config 
 sft_config = SFTConfig(
@@ -96,24 +26,19 @@ sft_config = SFTConfig(
     logging_steps=2,  # Frequency of logging training progress (log every 2 steps).
 )
 
-questions = [
-    "Give me an 1-sentence introduction of LLM.",
-    "Calculate 1+1-1",
-    "What's the difference between thread and process?"
-]
-
 if __name__ == "__main__":
     print("I can show you how to SFT:")
+    configs = load_config(config_path="src/sft_config.yaml")
 
     print("Define Checkpoint and device:")
-    checkpoint = "HuggingFaceTB/SmolLM2-135M"
-    device = "cpu" # for GPU usage or "cpu" for CPU usage
+    checkpoint = configs.checkpoint
+    device = configs.device # for GPU usage or "cpu" for CPU usage
 
     print("Load model and tokeniser:")
     model, tokenizer = load_model_and_tokenizer(checkpoint, device)
 
     print("Test model before SFT:")
-    test_model_with_questions(model, tokenizer, questions, 
+    test_model_with_questions(model, tokenizer, QUESTIONS, 
                             title="Base Model (After SFT) Output")
 
     print("Load dataset:")
@@ -135,5 +60,5 @@ if __name__ == "__main__":
 
     print("Test model after SFT:")
     sft_trainer.model.to(device)
-    test_model_with_questions(sft_trainer.model, tokenizer, questions, 
+    test_model_with_questions(sft_trainer.model, tokenizer, QUESTIONS, 
                             title="Base Model (After SFT) Output")
